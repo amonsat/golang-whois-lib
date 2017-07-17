17ba0ad83eacb8f5a362516d880d84e0b38adb73
Борис Мельников <amonsat@gmail.com>
2017-07-17 15:02:31 +0300

    Add cache

--- whois.go
+++ whois.go
@@ -17,6 +17,16 @@ const (
 	aliasWhoisServer  = ".whois-servers.net"
 )
 
+var (
+	CacheIANA  = make(map[string]string)
+	CacheWhois = make(map[string]string)
+)
+
+type server struct {
+	domain string
+	zone   string
+}
+
 func GetWhois(domain string) (string, error) {
 	return GetWhoisTimeout(domain, time.Second*5)
 }
@@ -41,8 +51,9 @@ func GetWhoisTimeout(domain string, timeout time.Duration) (string, error) {
 
 	var res string
 	for _, server := range servers {
-		res, err = GetWhoisData(domainUnicode, server, timeout)
+		res, err = GetWhoisData(domainUnicode, server.domain, timeout)
 		if CorrectWhoisInfo(res) {
+			CacheWhois[server.zone] = server.domain
 			log.Printf("Correct whois server: %v\n", server)
 			break
 		}
@@ -57,27 +68,37 @@ func GetWhoisTimeout(domain string, timeout time.Duration) (string, error) {
 	return res, err
 }
 
-func GetPossibleWhoisServers(domain string, timeout time.Duration) (whoisServers []string, err error) {
+func GetPossibleWhoisServers(domain string, timeout time.Duration) (whoisServers []*server, err error) {
 	parts := strings.Split(domain, ".")
 	lenDomain := len(parts)
 
 	topLevelDomain := parts[lenDomain-1]
 	if lenDomain > 2 {
 		secondLevelDomain := parts[lenDomain-2] + "." + parts[lenDomain-1]
-		whoisServers = append(whoisServers, "whois.nic."+secondLevelDomain)
-		whoisServers = append(whoisServers, "whois."+secondLevelDomain)
+		if val, ok := CacheWhois[secondLevelDomain]; ok {
+
+			whoisServers = append(whoisServers, &server{val, secondLevelDomain})
+		}
+		whoisServers = append(whoisServers, &server{"whois.nic." + secondLevelDomain, secondLevelDomain})
+		whoisServers = append(whoisServers, &server{"whois." + secondLevelDomain, secondLevelDomain})
 	}
 
-	whoisServers = append(whoisServers, "whois.nic."+topLevelDomain)
-	whoisServers = append(whoisServers, "whois."+topLevelDomain)
-	whoisServers = append(whoisServers, defaulWhoisServer)
-	whoisServers = append(whoisServers, topLevelDomain+aliasWhoisServer)
+	if val, ok := CacheWhois[topLevelDomain]; ok {
+		whoisServers = append(whoisServers, &server{topLevelDomain, val})
+	}
+	whoisServers = append(whoisServers, &server{"whois.nic." + topLevelDomain, topLevelDomain})
+	whoisServers = append(whoisServers, &server{"whois." + topLevelDomain, topLevelDomain})
+	whoisServers = append(whoisServers, &server{defaulWhoisServer, topLevelDomain})
+	whoisServers = append(whoisServers, &server{topLevelDomain + aliasWhoisServer, topLevelDomain})
 
-	if whoisFromIANA := GetWhoisServerFromIANA(topLevelDomain, timeout); whoisFromIANA != "" {
-		whoisServers = append(whoisServers, whoisFromIANA)
+	if val, ok := CacheIANA[topLevelDomain]; ok {
+		whoisServers = append(whoisServers, &server{val, topLevelDomain})
+	} else if whoisFromIANA := GetWhoisServerFromIANA(topLevelDomain, timeout); whoisFromIANA != "" {
+		whoisServers = append(whoisServers, &server{whoisFromIANA, topLevelDomain})
+		CacheIANA[topLevelDomain] = whoisFromIANA
 	}
 
-	log.Printf("possible whois server: %v\n", strings.Join(whoisServers, ", "))
+	// log.Printf("possible whois server: %v\n", strings.Join(whoisServers, ", "))
 	return
 }
 
