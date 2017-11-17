027c7883c50b78b8c2b116a55ba666002b29e282
Борис Мельников <amonsat@gmail.com>
2017-11-18 00:38:17 +0200

    Use sync.Map for concurrency

--- whois.go
+++ whois.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net"
 	"strings"
+	"sync"
 	"time"
 
 	"golang.org/x/net/idna"
@@ -19,8 +20,8 @@ const (
 )
 
 var (
-	CacheIANA  = make(map[string]string)
-	CacheWhois = make(map[string]string)
+	CacheIANA  sync.Map
+	CacheWhois sync.Map
 )
 
 var whoisServerTemplate = map[string]string{
@@ -94,7 +95,7 @@ func GetWhoisTimeout(domain string, timeout time.Duration) (string, error) {
 		}
 
 		if IsWhoisDataCorrect(res) {
-			CacheWhois[server.zone] = server.domain
+			CacheWhois.Store(server.zone, server.domain)
 			log.Printf("Correct whois server: %v\n", server.domain)
 			break
 		} else if ParseNofound(res) {
@@ -112,27 +113,26 @@ func GetPossibleWhoisServers(domain string, timeout time.Duration) (whoisServers
 	topLevelDomain := parts[lenDomain-1]
 	if lenDomain > 2 {
 		secondLevelDomain := parts[lenDomain-2] + "." + parts[lenDomain-1]
-		if val, ok := CacheWhois[secondLevelDomain]; ok {
-
-			whoisServers = append(whoisServers, &server{val, secondLevelDomain})
+		if val, ok := CacheWhois.Load(secondLevelDomain); ok {
+			whoisServers = append(whoisServers, &server{val.(string), secondLevelDomain})
 		}
 		whoisServers = append(whoisServers, &server{"whois.nic." + secondLevelDomain, secondLevelDomain})
 		whoisServers = append(whoisServers, &server{"whois." + secondLevelDomain, secondLevelDomain})
 	}
 
-	if val, ok := CacheWhois[topLevelDomain]; ok {
-		whoisServers = append(whoisServers, &server{topLevelDomain, val})
+	if val, ok := CacheWhois.Load(topLevelDomain); ok {
+		whoisServers = append(whoisServers, &server{topLevelDomain, val.(string)})
 	}
 	whoisServers = append(whoisServers, &server{"whois.nic." + topLevelDomain, topLevelDomain})
 	whoisServers = append(whoisServers, &server{"whois." + topLevelDomain, topLevelDomain})
 	whoisServers = append(whoisServers, &server{defaulWhoisServer, topLevelDomain})
 	whoisServers = append(whoisServers, &server{topLevelDomain + aliasWhoisServer, topLevelDomain})
 
-	if val, ok := CacheIANA[topLevelDomain]; ok {
-		whoisServers = append(whoisServers, &server{val, topLevelDomain})
+	if val, ok := CacheIANA.Load(topLevelDomain); ok {
+		whoisServers = append(whoisServers, &server{val.(string), topLevelDomain})
 	} else if whoisFromIANA := GetWhoisServerFromIANA(topLevelDomain, timeout); whoisFromIANA != "" {
 		whoisServers = append(whoisServers, &server{whoisFromIANA, topLevelDomain})
-		CacheIANA[topLevelDomain] = whoisFromIANA
+		CacheWhois.Store(topLevelDomain, whoisFromIANA)
 	}
 
 	return
